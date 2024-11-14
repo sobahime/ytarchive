23dfe2d5d4f0d331766e29e22b434f7852abd247
sobahime <augustusplatypus@gmail.com>
2024-11-15 00:51:02 +0100

    move style to css file, add ext colums

--- scripts/dbinit.sql
+++ scripts/dbinit.sql
@@ -1,28 +1,30 @@
 CREATE TABLE temp (data jsonb);
 
 CREATE TABLE video (
-	id			TEXT PRIMARY KEY,
-	title			TEXT,
-	-- thumbnails table
-	thumbnail		TEXT,
-	description		TEXT,
-	channel_id		TEXT,
-	channel_url		TEXT,
-	duration		INT,
-	view_count		INT,
-	webpage_url		TEXT,
-	-- tags table
-	-- subtitles table
-	comment_count		INT,
-	like_count		INT,
-	channel			TEXT,
-	channel_follower_count	INT,
-	uploader		TEXT,
-	uploader_id		TEXT,
-	uploader_url		TEXT,
-	upload_date		TEXT,
-	timestamp		INT,
-	fulltitle		TEXT,
-	fps			INT,
-	aspect_ratio		FLOAT
+    id                      TEXT PRIMARY KEY,
+    title                   TEXT,
+    -- thumbnails table
+    thumbnail               TEXT,
+    description             TEXT,
+    channel_id              TEXT,
+    channel_url             TEXT,
+    duration                INT,
+    view_count              INT,
+    webpage_url             TEXT,
+    -- tags table
+    -- subtitles table
+    comment_count           INT,
+    like_count              INT,
+    channel                 TEXT,
+    channel_follower_count  INT,
+    uploader                TEXT,
+    uploader_id             TEXT,
+    uploader_url            TEXT,
+    upload_date             TEXT,
+    timestamp               INT,
+    fulltitle               TEXT,
+    fps                     INT,
+    aspect_ratio            FLOAT,
+    ext                     TEXT,
+    thumbnail_ext           TEXT
 );
--- src/index.php
+++ src/index.php
@@ -6,108 +6,7 @@ require_once 'database.php';
     <head>
         <meta charset="utf-8">
         <title>media</title>
-	<style>
-html {
-    height: 100%;
-}
-body {
-	font-family: sans-serif;
-    background-image: url(noise.jpg);
-    background-size: cover;
-    color: white;
-    height: 95%;
-}
-.contenant {
-    height: 88vh;
-}
-a {
-	color: white;
-	text-decoration: none;
-}
-.video_list {
-    background-size: cover;
-    background-repeat: repeat-x;
-	display: flex;
-    flex-direction: column;
-	flex-wrap: wrap;
-    padding: 2em;
-    padding-bottom: 0;
-    padding-top: 0;
-    height: 100%;
-}
-.video_link {
-    background-color: rgba(0.1,0.1,0.1,0.8);
-	display: flex;
-    flex-direction: column;
-	padding: 4px;
-    margin: 0.2em;
-    text-align: center;
-    width: 32vh;
-    height: 26vh;
-}
-.thumbnail {
-    text-align: center;
-    width: 100%;
-    aspect-ratio: 16 / 9;
-	object-fit: contain;
-}
-.video_link_meta {
-    padding: 0;
-}
-.titre {
-    width: 100%;
-    height: 10%;
-}
-.titretexte {
-    position: fixed;
-    left: 50%;
-    transform: translateX(-50%);
-    margin: 0;
-    vertical-align: middle;
-    padding: 0;
-    font-size: 4em;
-    text-align: center;
-    mix-blend-mode: screen;
-}
-.titretexte.R {
-    color: #ff0000;
-}
-.titretexte.G {
-    color: #00ff00;
-}
-.titretexte.B {
-    color: #0000ff;
-}
-.titre:hover .R {
-  animation: shake 0.82s cubic-bezier(.36,.07,.80,.97) both infinite;
-  transform: translate(-50%, 0);
-}
-.titre:hover .G {
-  animation: shake 0.40s cubic-bezier(.36,.07,.19,.97) both infinite;
-  transform: translate(-50%, 0);
-}
-.titre:hover .B {
-  animation: shake 0.60s cubic-bezier(0,.07,.19,.97) both infinite;
-  transform: translate(-50%, 0);
-}
-@keyframes shake {
-  10%, 90% {
-    transform: translate(calc(-50% - 1px), 0);
-  }
-
-  20%, 80% {
-    transform: translate(calc(-50% - 2px), 0);
-  }
-
-  30%, 50%, 70% {
-    transform: translate(calc(-50% - 4px), 4px);
-  }
-
-  40%, 60% {
-    transform: translate(calc(-50% + 4px), 0);
-  }
-}
-</style>
+        <link rel="stylesheet" href="style.css" />
     </head>
     <body>
 <div class="titre">
@@ -128,7 +27,7 @@ foreach($data as $row) {
     $url_channel_escaped = 'channel.php?channel_id=' . htmlspecialchars($row["channel_id"]);
 	echo '<div class="video_link">';
 	echo '<a href="' . $url_escaped . '">';
-	echo '<img class="thumbnail" src="' . htmlspecialchars($row["thumbnail"]) . '"/>';
+	echo '<img class="thumbnail" src="content/' . htmlspecialchars($row["id"]) . '.webp"/>';
 	echo '</a>';
 //	echo '<b class="video_link_meta" href="' . $url_escaped . '">';
 	echo '<a class="title" href="' . $url_escaped . '"><strong>' . htmlspecialchars($row["title"]) . "</strong><br/></a>";
