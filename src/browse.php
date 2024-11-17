c4849db0675b25cac31ffe0b61e78d44b4065410
LMIJLM <neartree.no.fake@gmail.com>
2024-11-17 15:33:17 +0000

    finition front-end navbar + plein de petits tweaks (scroll smooth rapide amélioré + mini bugfix font etc...)

--- src/browse.php
+++ src/browse.php
@@ -61,14 +61,68 @@ foreach($data as $row) {
 ?>
 
 </div>
+<nav class="navigator">
+    <form action="search.php" method="get">
+        <div class="mainsearch">
+            <input type="search" id="search" name="q"/>
+            <button class="boutonmignon">search</button>
+        </div>
+            <h1 class="advancedtitle">search through</h1>
+            <div class="checkboxes navigo">
+                <div>
+                    <input type="checkbox" id="title" name="title" checked />
+                    <label for="title">title</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="channel" name="channel" checked />
+                    <label for="channel">channel</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="tags" name="tags" checked />
+                    <label for="tags">tags</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="description" name="description" />
+                    <label for="description">description</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="comments" name="comments" />
+                    <label for="comments">comments</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="date" name="date" />
+                    <label for="date">date (yyyy/mm/dd)</label>
+                </div>
 </div>
+            <h1 class="advancedtitle">sort by</h1>
+            <div class="checkboxes navigo">
+                <div>
+                    <input type="radio" id="sortdate" name="sortby" value="sortdate" />
+                    <label for="sortdate">date</label>
+                </div>
+                <div>
+                    <input type="radio" id="viewcount" name="sortby" value="viewcount" />
+                    <label for="viewcount">view count</label>
+                </div>
+                <div>
+                    <input type="radio" id="commentscount" name="sortby" value="commentscount" />
+                    <label for="commentscount">comments count</label>
+                </div>
+</div>
+            <label for="order"><h1 class="advancedtitle">order</h1></label>
+            <select name="order" id="order">
+                <option value="ascending">ascending</option>
+                <option value="descending">descending</option>
+            </select>
+            </form>
+</nav>
     <script>
         function transformScroll(event) {
   if (!event.deltaY) {
     return;
   }
 
-  event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
+  event.currentTarget.scrollLeft += (event.deltaY + event.deltaX)*4;
   event.preventDefault();
 }
 
--- src/search.php
+++ src/search.php
@@ -82,17 +82,114 @@ if (sizeof($columns) != 0) {
     }
 
 }
+$q = $_GET['q'] ?? '';
+$q = htmlspecialchars($q);
 ?>
 
 </div>
 </div>
+<nav class="navigator">
+    <form action="search.php" method="get">
+        <div class="mainsearch">
+        <input type="search" id="search" name="q" value="<?php echo $q; ?>">
+            <button class="boutonmignon">search</button>
+        </div>
+            <h1 class="advancedtitle">search through</h1>
+            <div class="checkboxes navigo">
+                <div>
+                    <input type="checkbox" id="title" name="title" 
+                        <?php
+                            if (isset($_GET['title'])) {echo 'checked';}
+                        ?> 
+                    />
+                    <label for="title">title</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="channel" name="channel"
+                        <?php
+                            if (isset($_GET['channel'])) {echo 'checked';}
+                        ?> 
+                    />
+                    <label for="channel">channel</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="tags" name="tags" 
+                        <?php
+                            if (isset($_GET['tags'])) {echo 'checked';}
+                        ?> 
+                    />
+                    <label for="tags">tags</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="description" name="description" 
+                        <?php
+                            if (isset($_GET['description'])) {echo 'checked';}
+                        ?> 
+                    />
+                    <label for="description">description</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="comments" name="comments"
+                        <?php
+                            if (isset($_GET['comments'])) {echo 'checked';}
+                        ?> 
+                    />
+                    <label for="comments">comments</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="date" name="date" 
+                        <?php
+                            if (isset($_GET['date'])) {echo 'checked';}
+                        ?> 
+                    />
+                    <label for="date">date (yyyy/mm/dd)</label>
+                </div>
+</div>
+            <h1 class="advancedtitle">sort by</h1>
+            <div class="checkboxes navigo">
+                <div>
+                    <input type="radio" id="sortdate" name="sortby" value="sortdate" 
+                        <?php
+                            echo ($_REQUEST['sortby'] == 'sortdate') ? 'checked' : '';
+                        ?> 
+                    />
+                    <label for="sortdate">date</label>
+                </div>
+                <div>
+                    <input type="radio" id="viewcount" name="sortby" value="viewcount" 
+                        <?php
+                            echo ($_REQUEST['sortby'] == 'viewcount') ? 'checked' : '';
+                        ?> 
+                    />
+                    <label for="viewcount">view count</label>
+                </div>
+                <div>
+                    <input type="radio" id="commentscount" name="sortby" value="commentscount" 
+                        <?php
+                            echo ($_REQUEST['sortby'] == 'commentscount') ? 'checked' : '';
+                        ?> 
+                    />
+                    <label for="commentscount">comments count</label>
+                </div>
+</div>
+            <label for="order"><h1 class="advancedtitle">order</h1></label>
+            <select name="order" id="order">
+                <option value="ascending">ascending</option>
+                <option value="descending" 
+<?php 
+                            echo ($_REQUEST['order'] == 'descending') ? 'selected' : '';
+?>
+                >descending</option>
+            </select>
+            </form>
+</nav>
     <script>
         function transformScroll(event) {
   if (!event.deltaY) {
     return;
   }
 
-  event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
+  event.currentTarget.scrollLeft += (event.deltaY + event.deltaX)*4;
   event.preventDefault();
 }
 
